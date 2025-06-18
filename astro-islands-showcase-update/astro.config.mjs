74ac30d4ab38a91db2218b1f41eed9b1e102304a
Vlado Cavric <vlado.cavric@htecgroup.com>
2025-06-18 20:31:23 +0200

    course finished

---
+++ astro-islands-showcase-update/astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import vue from '@astrojs/vue';
+
+import tailwindcss from '@tailwindcss/vite';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), vue()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
